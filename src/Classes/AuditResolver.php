0470aa1a07981fbb5312b59dfd8aa05801572500
ahmedeidandq <aeid@qurtobasolutions.com>
2022-01-05 23:51:18 +0200

    Fix

---
+++ src/Classes/AuditResolver.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Auditeur\Auditeur\Classes;
+
+use ErrorException ; 
+use BadFunctionCallException ;
+use OwenIt\Auditing\Models\Audit;
+
+class AuditResolver {
+
+	private $audit ;
+
+	public function __construct(Audit $audit) 
+	{
+		$this ->audit = $audit ; 
+	}
+
+	public function resolve()
+	{
+
+		return [ 
+
+			'Authenticatable' => $this ->audit ->user ? $this ->audit ->user ->{config('auditeur.user_types') ['attribute']} : '' , 
+			'Event'  => config('auditeur.events') [$this ->audit ->event] , 
+			'Auditable' => config('auditeur.auditable_types') [$this ->audit ->auditable_type]['name'] ?? $this ->audit ->auditable_type , 
+			'o' => $this ->audit ->_parse($this ->audit ->old_values) ,
+			'n' => $this ->audit ->_parse($this ->audit ->new_values) , 
+			'Client' => $this ->audit ->user_agent , 
+			'IP Address' => $this ->audit ->ip_address , 
+			'Time' => $this ->audit ->created_at ->format('Y-m-d l') ,
+		] ;
+
+	}
+
+	public function _parse(array $o)
+	{
+
+		$r = [] ;
+
+		foreach ($o as $k => $v) {
+
+			try {
+				if (is_array(config('auditeur.auditable_types') [$this ->audit ->auditable_type]['attributes'][$k])) {
+
+					// dd(config('auditeur.auditable_types') [$this ->audit ->auditable_type]['attributes'][$k]['name']) ; 
+					$r [config('auditeur.auditable_types') [$this ->audit ->auditable_type]['attributes'][$k]['name']] = $this ->audit ->_attatch(config('auditeur.auditable_types') [$this ->audit ->auditable_type]['attributes'][$k]) ;
+
+				} else {
+					$r [config('auditeur.auditable_types') [$this ->audit ->auditable_type]['attributes'][$k]] = $v;
+
+				}
+
+			} catch (ErrorException $e) {
+
+				$r [$k] = $v ; 
+
+			} catch (BadFunctionCallException $e) {
+
+				$r [config('auditeur.auditable_types') [$this ->audit ->auditable_type]['attributes'][$k]['name']] = $v ; 
+			}
+		}
+
+		return $r ; 
+	}
+
+	public function _attatch(array $r)
+	{
+
+
+		if (! method_exists($this ->audit ->auditable, $r ['relation']['name'])) throw new BadFunctionCallException() ;
+		return $this ->audit ->auditable ->{$r ['relation']['name']} ->{$r ['relation']['attribute']} ;
+	}
+
+}
--- src/Http/Resources/AuditResource.php
+++ src/Http/Resources/AuditResource.php
@@ -2,66 +2,15 @@
 
 namespace Auditeur\Auditeur\Http\Resources;
 
+use Auditeur\Auditeur\Classes\AuditResolver;
 use Illuminate\Http\Resources\Json\JsonResource;
-use ErrorException ; 
-use BadFunctionCallException ;
 
 class AuditResource extends JsonResource {
 
 
 	public function toArray($request)
 	{
-
-		// dd(config('auditeur.user_types')) ;
-		return [ 
-			'Authenticatable' => $this ->user ? $this ->user ->{config('auditeur.user_types') ['attribute']} : '' , 
-			'Event'  => config('auditeur.events') [$this ->event] , 
-			'Auditable' => config('auditeur.auditable_types') [$this ->auditable_type]['name'] ?? $this ->auditable_type , 
-			'o' => $this ->_parse($this ->old_values) ,
-			'n' => $this ->_parse($this ->new_values) , 
-			'Client' => $this ->user_agent , 
-			'IP Address' => $this ->ip_address , 
-			'Time' => $this ->created_at ->format('Y-m-d l') ,
-		] ;
-	}
-
-	public function _parse(array $o)
-	{
-
-		$r = [] ;
-
-		foreach ($o as $k => $v) {
-
-			try {
-				if (is_array(config('auditeur.auditable_types') [$this ->auditable_type]['attributes'][$k])) {
-
-					// dd(config('auditeur.auditable_types') [$this ->auditable_type]['attributes'][$k]['name']) ; 
-					$r [config('auditeur.auditable_types') [$this ->auditable_type]['attributes'][$k]['name']] = $this ->_attatch(config('auditeur.auditable_types') [$this ->auditable_type]['attributes'][$k]) ;
-
-				} else {
-					$r [config('auditeur.auditable_types') [$this ->auditable_type]['attributes'][$k]] = $v;
-
-				}
-
-			} catch (ErrorException $e) {
-
-				$r [$k] = $v ; 
-
-			} catch (BadFunctionCallException $e) {
-
-				$r [config('auditeur.auditable_types') [$this ->auditable_type]['attributes'][$k]['name']] = $v ; 
-			}
-		}
-
-		return $r ; 
-	}
-
-	public function _attatch(array $r)
-	{
-
-
-		if (! method_exists($this ->auditable, $r ['relation']['name'])) throw new BadFunctionCallException() ;
-		return $this ->auditable ->{$r ['relation']['name']} ->{$r ['relation']['attribute']} ;
+		return (new AuditResolver($this)) ->resolve() ;
 	}
 
 
